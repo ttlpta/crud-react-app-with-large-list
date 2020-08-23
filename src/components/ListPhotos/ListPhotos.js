1cfb49e4afab429d494888b20e27ac307b6db2a8
anhpt <tuananh200898@gmail.com>
2020-08-23 13:19:32 +0700

    up

--- src/components/ListPhotos/ListPhotos.js
+++ src/components/ListPhotos/ListPhotos.js
@@ -4,49 +4,85 @@ import AutoSizer from 'react-virtualized-auto-sizer'
 import _chunk from 'lodash/chunk'
 
 import ListPhotosWrapper from './ListPhotosWrapper'
+import useLongPress from '../../hooks/useLongPress'
 
 const GUTTER_SIZE = 15
 const ITEM_PER_ROW = 5
-const ROW_HEIGHT = 120 + GUTTER_SIZE
+const ROW_HEIGHT = 220 + GUTTER_SIZE
 const _columnWidth = width => width / 5 - GUTTER_SIZE
-
-const Cell = items => ({ columnIndex, rowIndex, style }) => {
-  const overWriteStyle = {
-    ...style,
-    left: style.left + GUTTER_SIZE,
-    top: style.top + GUTTER_SIZE,
-    width: style.width - GUTTER_SIZE,
-    height: style.height - GUTTER_SIZE
-  }
-
-  return (
-    <div
-      key={`${columnIndex}-${rowIndex}`}
-      className='list-photo__grid__item'
-      style={overWriteStyle}
-    >
-      <img src={items[rowIndex][columnIndex]} />
-    </div>
-  )
-}
-
-const GridRenderer = items => ({ height, width }) => (
-  <Grid
-    className='list-photo__grid'
-    columnCount={ITEM_PER_ROW}
-    columnWidth={_columnWidth(width)}
-    height={height}
-    width={width}
-    rowCount={items.length}
-    rowHeight={ROW_HEIGHT}
-    autoHeight={true}
-  >
-    {Cell(items)}
-  </Grid>
-)
 
 export default function ListPhotos ({ items }) {
   const [images, setImages] = useState([])
+  const onLongPress = () => {
+    const yes = window.confirm('Are you sure to delete this image?')
+  }
+
+  const onClick = () => {
+    console.log('click is triggered')
+  }
+
+  const longPressEvent = useLongPress(onLongPress, onClick)
+
+  const CheckBox = function (props) {
+    const [checked, check] = useState(false)
+
+    const handleCheck = () => {
+      check(!checked)
+    }
+
+    return (
+      <div
+        className={`list-photo__grid__item__checkbox ${checked ? 'show' : ''}`}
+      >
+        <input type='checkbox' onChange={handleCheck} />
+      </div>
+    )
+  }
+  const Cell = items => ({ columnIndex, rowIndex, style }) => {
+    const overWriteStyle = {
+      ...style,
+      left: style.left + GUTTER_SIZE,
+      top: style.top + GUTTER_SIZE,
+      width: style.width - GUTTER_SIZE,
+      height: style.height - GUTTER_SIZE
+    }
+
+    return (
+      <div
+        key={`${columnIndex}-${rowIndex}`}
+        className='list-photo__grid__item'
+        {...longPressEvent}
+        title='Hold to immediately delete'
+        style={overWriteStyle}
+      >
+        <CheckBox />
+        <div className='list-photo__grid__item__image'>
+          <img src={items[rowIndex][columnIndex]} />
+        </div>
+        <div className='list-photo__grid__item__info'>
+          <p className='list-photo__grid__item__info__imageName'>
+            read-12354.jpg
+          </p>
+          <p className='list-photo__grid__item__info__category'>Nature</p>
+        </div>
+      </div>
+    )
+  }
+
+  const GridRenderer = items => ({ height, width }) => (
+    <Grid
+      className='list-photo__grid'
+      columnCount={ITEM_PER_ROW}
+      columnWidth={_columnWidth(width)}
+      height={height}
+      width={width}
+      rowCount={items.length}
+      rowHeight={ROW_HEIGHT}
+      autoHeight={true}
+    >
+      {Cell(items)}
+    </Grid>
+  )
 
   useEffect(() => {
     const chunkItems = _chunk(items, ITEM_PER_ROW)
--- src/components/ListPhotos/ListPhotosWrapper.js
+++ src/components/ListPhotos/ListPhotosWrapper.js
@@ -11,13 +11,47 @@ export default styled.div`
 
       &__item {
         text-align: center;
-        background-color: #ccc3;
+
         cursor: pointer;
         &:hover {
           background-color: #cccccc75;
+          .list-photo__grid__item__checkbox {
+            display: block;
+          }
         }
-        img {
-          height: 100%;
+        &__image {
+          width: 100%;
+          height: 70%;
+          background-color: #ccc3;
+          img {
+            height: 100%;
+          }
+        }
+        &__info {
+          margin-top: 10px;
+          p {
+            margin: 0;
+          }
+          &__imageName {
+            font-weight: bold;
+          }
+          &__category {
+            font-size: 0.7rem;
+          }
+        }
+        &__checkbox {
+          position: absolute;
+          width: 100%;
+          text-align: left;
+          display: none;
+          background-image: linear-gradient(#04040440, #10101000);
+          &.show {
+            display: block;
+          }
+          input {
+            width: 18px;
+            height: 18px;
+          }
         }
       }
     }
